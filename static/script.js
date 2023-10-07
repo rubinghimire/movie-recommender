33784fd2853f03ef5565e4514c47848c3610cf56
rubinghimire <rubin.ghimire@gmail.com>
2023-10-07 21:47:40 +0545

    UI improvements

--- static/script.js
+++ static/script.js
@@ -2,22 +2,18 @@
 function getRecommendations() {
     console.log('Function called');
     const movieInput = document.getElementById('movieInput');
-    const searchBox = document.querySelector('.search-box');
+    const message = document.querySelector('.message');
     const recommendationsDiv = document.getElementById('recommendations');
-    const button = document.querySelector('.search-box button');
 
     // Get the user input (movie title)
     const movieTitle = movieInput.value.trim();
-
-    // Create and append a loading message element
-    const loadingMessage = document.createElement('h4');
-    loadingMessage.innerText = 'Please wait for the server\'s response.';
-    searchBox.appendChild(loadingMessage);
+    // Ask user to wait
+    message.innerText = 'Please wait for the server\'s response.';
 
     // Check if the input is empty
     if (!movieTitle) {
         alert('Please enter a movie title.');
-        loadingMessage.remove(); // Remove the loading message
+        message.innerText = ""; // Remove the message
         return;
     }
 
@@ -25,8 +21,8 @@ function getRecommendations() {
     fetch(`https://movie-recommender-z51m.onrender.com/recommend/${movieTitle}`)
         .then(response => response.json())
         .then(data => {
-            // Remove the loading message
-            loadingMessage.remove();
+            // Erase the message
+            message.innerText = "";
 
             // Display recommendations in the recommendationsDiv
             recommendationsDiv.innerHTML = '';
@@ -37,7 +33,7 @@ function getRecommendations() {
             });
         })
         .catch(error => {
-            // Update the loading message with the error
-            loadingMessage.innerText = 'Error: ' + error;
+            // Update the message with the error
+            message.innerText = 'Error: ' + error;
         });
 }
