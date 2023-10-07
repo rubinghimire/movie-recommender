fd120fa78f769b5d7281346f90844b359bce745a
rubinghimire <rubin.ghimire@gmail.com>
2023-10-07 14:17:29 +0545

    enabling CORS

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import argparse
 import numpy as np
 import pandas as pd
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 app.debug = True
 
 
